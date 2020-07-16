a4ba9bf57295207acab108f1e7faa2b03a2a2bdd
Alexandre <alexandrecarlos2@gmail.com>
2020-07-16 17:49:02 -0300

    atualização curso identity

---
+++ AspNetIdentity/Modulo2/ByteBank.Forum/ByteBank.Forum/Startup.cs
@@ -0,0 +1,91 @@
+﻿using ByteBank.Forum.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Microsoft.AspNet.Identity.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ByteBank.Forum.App_Start.Identity;
+using Microsoft.Ajax.Utilities;
+using Microsoft.Owin.Security.Cookies;
+
+[assembly: OwinStartup(typeof(ByteBank.Forum.Startup))]
+namespace ByteBank.Forum
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder builder)
+        {
+            builder.CreatePerOwinContext<DbContext>(() => new IdentityDbContext<UsuarioAplicacao>("DefaultConnection"));
+
+            builder.CreatePerOwinContext<IUserStore<UsuarioAplicacao>>(
+
+                (opcoes, contextoOwin) =>
+                {
+                    var dbContext = contextoOwin.Get<DbContext>();
+                    return new UserStore<UsuarioAplicacao>(dbContext);
+                });
+
+            builder.CreatePerOwinContext<UserManager<UsuarioAplicacao>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userStore = contextoOwin.Get<IUserStore<UsuarioAplicacao>>();
+                    var userManager = new UserManager<UsuarioAplicacao>(userStore);
+
+                    var userValidator = new UserValidator<UsuarioAplicacao>(userManager);
+
+                    userValidator.RequireUniqueEmail = true;
+
+                    userManager.UserValidator = userValidator;
+
+                    userManager.PasswordValidator = new SenhaValidador()
+                    {
+                        TamanhoRequerido = 6,
+                        ObrigatorioCaracteresEspeciais = true,
+                        ObrigatorioDigitos = true,
+                        ObrigatorioLowerCase = true,
+                        ObrigatorioUpperCase = true
+
+                    };
+
+                    userManager.EmailService = new EmailServico();
+
+                    var dataProtectioProvider = opcoes.DataProtectionProvider;
+
+                    var dataProtectionProviderCreated = dataProtectioProvider.Create("ByteBank.Forum");
+
+                    userManager.UserTokenProvider = new DataProtectorTokenProvider<UsuarioAplicacao>(dataProtectionProviderCreated);
+
+                    userManager.MaxFailedAccessAttemptsBeforeLockout = 3;
+                    userManager.DefaultAccountLockoutTimeSpan = TimeSpan.FromMinutes(1);
+                    userManager.UserLockoutEnabledByDefault = true;
+
+                    return userManager;
+                });
+
+            builder.CreatePerOwinContext<SignInManager<UsuarioAplicacao, string>>(
+                (opcoes, contextoOwin) =>
+                {
+                    var userManager = contextoOwin.Get<UserManager<UsuarioAplicacao>>();
+                    var signInManager =
+                        new SignInManager<UsuarioAplicacao, string>(
+                            userManager,
+                            contextoOwin.Authentication);
+
+                    return signInManager;
+                });
+
+            builder.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie
+
+            });
+        }
+    }
+}
