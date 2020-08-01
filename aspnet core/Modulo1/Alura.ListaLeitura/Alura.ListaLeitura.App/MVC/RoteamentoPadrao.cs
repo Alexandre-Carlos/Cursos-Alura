bfb2ac246cc6b00d6a26ce9fc2463d3143f98118
Alexandre <alexandrecarlos2@gmail.com>
2020-08-01 07:57:29 -0300

    atualização de pastas e cursos

---
+++ aspnet core/Modulo1/Alura.ListaLeitura/Alura.ListaLeitura.App/MVC/RoteamentoPadrao.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App.MVC
+{
+    public class RoteamentoPadrao
+    {
+        public static Task TratamentoPadrao(HttpContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ aspnet core/Modulo1/Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,70 @@
+﻿using Alura.ListaLeitura.App.Logica;
+using Alura.ListaLeitura.App.MVC;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseMvcWithDefaultRoute();
+
+
+
+            //var builder = new RouteBuilder(app);
+            //builder.MapRoute("{controller}/{action}", RoteamentoPadrao.TratamentoPadrao);
+            //builder.MapRoute("{controller}/{action}/{nome}/{autor}", RoteamentoPadrao.TratamentoPadrao);
+
+
+            //builder.MapRoute("Livros/ParaLer", LivrosLogica.Paraler);
+            //builder.MapRoute("Livros/Lendo", LivrosLogica.Lendo);
+            //builder.MapRoute("Livros/Lidos", LivrosLogica.Lidos);
+            //builder.MapRoute("Livros/Detalhes/{id:int}", LivrosLogica.Detalhes);
+            //builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", CadastroLogica.NovoLivro);
+            //builder.MapRoute("Cadastro/ExibeFormulario", CadastroLogica.ExibeFormulario);
+            //builder.MapRoute("Cadastro/Incluir", CadastroLogica.Incluir);
+
+            //var rotas = builder.Build();
+
+            //app.UseRouter(rotas);
+
+            //app.Run(Roteamento);
+        }
+
+        
+        //public Task Roteamento(HttpContext context)
+        //{
+        //    var _repo = new LivroRepositorioCSV();
+        //    var caminhosAtendidos = new Dictionary<string, RequestDelegate>
+        //    {
+        //        { "/Livros/ParaLer", LivrosParaLer },
+        //        {  "/Livros/Lendo", LivrosLendo },
+        //        { "/Livros/Lidos", LivrosLidos }
+
+        //    };
+
+        //    if (caminhosAtendidos.ContainsKey(context.Request.Path))
+        //    {
+        //        var metodo = caminhosAtendidos[context.Request.Path];
+        //        return metodo.Invoke(context);
+        //    }
+
+
+        //    context.Response.StatusCode = 404;
+        //    return context.Response.WriteAsync("Caminho inexistente.");
+
+        //}
+
+        
+    }
+}
